ac0a59f357c3e6f053bfa6cb159791fc04bdba12
DeepeshRohit <rdeep6597@gmail.com>
2022-07-01 15:10:12 +0530

    First Assignment updated

--- JavaAssignments/src/Tax.java
+++ JavaAssignments/src/Tax.java
@@ -3,6 +3,7 @@ public class Tax {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Enter Anual Salary ");
 		Scanner s=new Scanner(System.in);
 		
         double ctc=s.nextDouble();
