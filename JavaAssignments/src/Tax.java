869556c01dc01751d11df0f9f73ddc328b4c7c0b
DeepeshRohit <rdeep6597@gmail.com>
2022-07-01 15:15:10 +0530

    update tax assignment

--- JavaAssignments/src/Tax.java
+++ JavaAssignments/src/Tax.java
@@ -11,17 +11,17 @@ public class Tax {
             System.out.println("tax payable in % is : NILL ");
         }
         else if(ctc>180000&&ctc<=300000) {
-            System.out.println(" tax payable in % is : 10 ");
+            System.out.println(" tax payable in % is : 10 Amoout :" +((ctc*10)/100));
         }
         else if(ctc>300000&&ctc<=500000) {
-            System.out.println(" tax payable in % is : 20 ");
+            System.out.println(" tax payable in % is : 20 Amoout :"+((ctc*30)/100));
         }
         else if(ctc>500000&&ctc<=1000000) {
-            System.out.println(" tax payable in % is : 30 ");
+            System.out.println(" tax payable in % is : 30 Amoout : "+((ctc*30)/100));
         }
         else{
 
-            System.out.println("tax payable in % is : 40 ");
+            System.out.println("tax payable in % is : 40 Amoout : "+((ctc*40)/100));
         }
 	}
 
