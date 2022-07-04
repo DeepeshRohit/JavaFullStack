6ff03dbee3f6969660506b950a7763c5eed20d3b
DeepeshRohit <rdeep6597@gmail.com>
2022-07-04 10:57:30 +0530

    Assignmnet 3 Updated

---
+++ JavaAssignment3/src/SecondJavaString.java
@@ -0,0 +1,13 @@
+
+public class SecondJavaString {
+
+    public static void main(String[] args) {
+
+            {
+                String s = "Hello ";
+
+                s = s.concat("How are You?");  //string concatenation
+                System.out.println(s);
+            }
+        }
+    }
