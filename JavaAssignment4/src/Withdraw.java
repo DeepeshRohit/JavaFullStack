f55369501cfe396702fe277d0747a3c2fa47b5a1
DeepeshRohit <rdeep6597@gmail.com>
2022-07-05 13:11:03 +0530

    StatementUpdate for Assignment 4

--- JavaAssignment4/src/Withdraw.java
+++ JavaAssignment4/src/Withdraw.java
@@ -3,30 +3,32 @@ import java.util.Scanner;
 public class Withdraw{
    
 	public static void main(String args[]) {
-		 double balance = 2000, withdrawAmount;
+		 double balance = 2000.0, withdrawAmount;
 
 	        Scanner sc=new Scanner(System.in);
 	       
 	        System.out.println("Enter Withdraw an amount ");
 	        withdrawAmount=sc.nextDouble();
-	        balance -= withdrawAmount;
+	        
 		
 	      try {
 	    	  if (withdrawAmount<=0) {
 	         throw new IllegalBankTransactionException("you can not witdraw negative or zero amount ");
 	         }
 	    	  else if(withdrawAmount>balance)
-	    	  {
+	    	  { 
 	    		  throw new InsufficientBalanceException("you can not Withdraw an amount more than your Balance");
 	    	  }
-	    	  else { System.out.println("Transaction Successful ");
-	    	  System.out.println("Your Balance is : "+balance);}
-	    	  
+	    	  else {
+	    		  System.out.println("Transaction Successfull your Balance is");
+	    	  System.out.println(balance -= withdrawAmount);
+	    	  }
 	      } catch(IllegalBankTransactionException e) {
 	         System.out.println(e);
 	         }
 	        catch(InsufficientBalanceException e) {
 	 	         System.out.println(e);
 	      }
+	     
 	   }
 }                 
